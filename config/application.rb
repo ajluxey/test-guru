b327052c8189c942743d9aa53978988c3d2975f3
ajluxey <v_koralina@list.ru>
2021-05-25 18:19:48 +0300

    delete useless string

--- config/application.rb
+++ config/application.rb
@@ -18,9 +18,8 @@ module TestGuru
     #
     config.time_zone = "Moscow"
 
-    config.i18n.load_path = Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
     config.i18n.default_locale = :ru
-    puts config.i18n.t('hello').inspect # Isn't working, idk y 
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end

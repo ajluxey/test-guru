76a309bbc42fe0150c0771b11b93ae39bd90ca8a
ajluxey <v_koralina@list.ru>
2021-06-17 01:12:25 +0300

    add methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.all_by_category(cat_title)
+    category = Category.find_by(title: cat_title)
+    Test.where(category_id: category.id).order(:title, :desc) if category
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def get_tests_with_level(n)
+    Test.find_by(level: n)
+  end
 end

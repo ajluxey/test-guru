fe337694bea89d8ccc47d337661035c89866cfd7
ajluxey <v_koralina@list.ru>
2021-06-20 12:32:18 +0300

    space add

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   def get_passed_tests_with_level(n)
     Test.joins('JOIN users_start_tests ON users_start_tests.test_id = tests.id')
         .joins('JOIN users ON users_start_tests.user_id = users.id')
-        .where(users: { id: self.id})
+        .where(users: { id: self.id })
         .where(level: n)
   end
 end

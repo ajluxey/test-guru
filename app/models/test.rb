93e1f80d66e08a1b0792a17ee8b466c199e9d2d1
ajluxey <v_koralina@list.ru>
2021-06-29 01:13:22 +0300

    hw for check

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 class Test < ApplicationRecord
+  has_many :questions
+  belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_and_belongs_to_many :is_started_by_users, join_table: 'users_start_tests', class_name: 'User'
+
   def self.all_by_category(cat_title)
     Test.joins('JOIN categories ON tests.category_id = categories.id')
         .where(categories: { title: cat_title })
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
+  has_and_belongs_to_many :started_tests, join_table: 'users_start_tests', class_name: 'Test'
+
   def get_passed_tests_with_level(n)
     Test.joins('JOIN users_start_tests ON users_start_tests.test_id = tests.id')
         .where(users_start_tests: { user_id: self.id })

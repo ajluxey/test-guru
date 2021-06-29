19e0884d380d4dd69502a99961e676294afd72d3
ajluxey <v_koralina@list.ru>
2021-06-30 02:37:45 +0300

    rework seed after adding relationship association

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,71 +8,71 @@
 
 user = User.create!(login: 'ajluxey', password: 'qwerty')
 categories = Category.create!([{ title: 'Ruby' }, { title: 'Алгоритмы' }])
-tests = Test.create!([{ title: 'Основы Ruby', category_id: categories[0].id, author_id: user.id },
-                      { title: 'Сортировки', level: 2, category_id: categories[1].id, author_id: user.id },
-                      { title: 'Ruby - Экспертный тест', level: 10, category_id: categories[0].id, author_id: user.id },
-                      { title: 'Алгоритмы сжатия информации', level: 2, category_id: categories[1].id, author_id: user.id }])
+tests = Test.create!([{ title: 'Основы Ruby', category: categories[0], author: user },
+                      { title: 'Сортировки', level: 2, category: categories[1], author: user },
+                      { title: 'Ruby - Экспертный тест', level: 10, category: categories[0], author: user },
+                      { title: 'Алгоритмы сжатия информации', level: 2, category: categories[1], author: user }])
 
-questions_r = Question.create!([{ body: 'Вы знаете Ruby?', test_id: tests[0].id },
-                                { body: 'Ruby - ето чьто?', test_id: tests[0].id },
-                                { body: 'Какая команда отвечает за вывод?', test_id: tests[0].id },
-                                { body: 'Вы эксперт в Ruby?', test_id: tests[2].id },
-                                { body: 'Точно?', test_id: tests[2].id }])
+questions_r = Question.create!([{ body: 'Вы знаете Ruby?', test: tests[0] },
+                                { body: 'Ruby - ето чьто?', test: tests[0] },
+                                { body: 'Какая команда отвечает за вывод?', test: tests[0] },
+                                { body: 'Вы эксперт в Ruby?', test: tests[2] },
+                                { body: 'Точно?', test: tests[2] }])
 
 questions_s = Question.create!([{ body: 'Что такое сортировка?',
-                                  test_id: tests[1].id },
+                                  test: tests[1] },
                                 { body: 'Есть ли алгоритм сортировки "пузырьком"?',
-                                  test_id: tests[1].id },
+                                  test: tests[1] },
                                 { body: 'Стали бы вы использовать алгоритм сортировки пузырьком на больших данных?',
-                                  test_id: tests[1].id },
+                                  test: tests[1] },
                                 { body: 'Данные можно сжать?',
-                                  test_id: tests[3].id },
+                                  test: tests[3] },
                                 { body: 'Как бы вы сжимали данные?',
-                                  test_id: tests[3].id }])
+                                  test: tests[3] }])
 
-Answer.create!([{ body: 'Да', correct: true, question_id: questions_r[0].id },
-                { body: 'Нет', question_id: questions_r[0].id }])
+Answer.create!([{ body: 'Да', correct: true, question: questions_r[0] },
+                { body: 'Нет', question: questions_r[0] }])
 
-Answer.create!([{ body: 'Язык программирования', correct: true, question_id: questions_r[1].id },
-                { body: 'Слово', question_id: questions_r[1].id },
-                { body: 'Рубин с английского', question_id: questions_r[1].id }])
+Answer.create!([{ body: 'Язык программирования', correct: true, question: questions_r[1] },
+                { body: 'Слово', question: questions_r[1] },
+                { body: 'Рубин с английского', question: questions_r[1] }])
 
-Answer.create!([{ body: 'puts', correct: true, question_id: questions_r[2].id },
-                { body: 'gets', question_id: questions_r[2].id },
-                { body: 'inspect', question_id: questions_r[2].id }])
+Answer.create!([{ body: 'puts', correct: true, question: questions_r[2] },
+                { body: 'gets', question: questions_r[2] },
+                { body: 'inspect', question: questions_r[2] }])
 
-Answer.create!([{ body: 'Да', correct: true, question_id: questions_r[3].id },
-                { body: 'Нет', question_id: questions_r[3].id }])
+Answer.create!([{ body: 'Да', correct: true, question: questions_r[3] },
+                { body: 'Нет', question: questions_r[3] }])
 
-Answer.create!([{ body: 'Да', correct: true, question_id: questions_r[4].id },
-                { body: 'Нет', question_id: questions_r[4].id }])
+Answer.create!([{ body: 'Да', correct: true, question: questions_r[4] },
+                { body: 'Нет', question: questions_r[4] }])
 
-Answer.create!([{ body: 'Алгоритм упорядочивания данных', correct: true, question_id: questions_s[0].id },
-                { body: 'Не знаю', question_id: questions_s[0].id }])
+Answer.create!([{ body: 'Алгоритм упорядочивания данных', correct: true, question: questions_s[0] },
+                { body: 'Не знаю', question: questions_s[0] }])
 
-Answer.create!([{ body: 'Да', correct: true, question_id: questions_s[1].id },
-                { body: 'Нет', question_id: questions_s[1].id }])
+Answer.create!([{ body: 'Да', correct: true, question: questions_s[1] },
+                { body: 'Нет', question: questions_s[1] }])
 
-Answer.create!([{ body: 'Нет', correct: true, question_id: questions_s[2].id },
-                { body: 'Да', question_id: questions_s[2].id }])
+Answer.create!([{ body: 'Нет', correct: true, question: questions_s[2] },
+                { body: 'Да', question: questions_s[2] }])
 
-Answer.create!([{ body: 'Да', correct: true, question_id: questions_s[3].id },
-                { body: 'нет', question_id: questions_s[3].id }])
+Answer.create!([{ body: 'Да', correct: true, question: questions_s[3] },
+                { body: 'нет', question: questions_s[3] }])
 
-Answer.create!([{ body: 'С помощью программ и алгоритмов', correct: true, question_id: questions_s[4].id },
-                { body: 'Руками со всей силы', question_id: questions_s[4].id }])
+Answer.create!([{ body: 'С помощью программ и алгоритмов', correct: true, question: questions_s[4] },
+                { body: 'Руками со всей силы', question: questions_s[4] }])
 
 biba = User.create!(login: 'biba', password: 'qwerty')
 boba = User.create!(login: 'boba', password: 'qwerty')
 
 tests.each do |test|
-  user.tests << test
-  biba.tests << test
+  user.started_tests << test
+  biba.started_tests << test
   # UserStartTest.create!(user_id: user.id, test_id: test.id, passed: true)
   # UserStartTest.create!(user_id: biba.id, test_id: test.id, passed: [true, false].sample)
 end
 
 tests.sample(2).each do |test|
-  boba.tests << test
+  boba.started_tests << test
   # UserStartTest.create!(user_id: boba.id, test_id: test.id, passed: [true, false].sample)
 end

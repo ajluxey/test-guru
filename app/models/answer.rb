625fbe27f8c91deb34ce58ab7b7c1b9dd15ffd98
ajluxey <v_koralina@list.ru>
2021-07-03 19:42:27 +0300

    rework answer count validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,13 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validate :validate_answer_count
+  validate :validate_answer_count, on: :create
 
   scope :correct, -> { where(correct: true) }
 
   private
 
   def validate_answer_count
-    errors.add(:question, 'question can contain max 4 answers') if question.answers.count == 4
+    errors.add(:question, 'question can contain max 4 answers') if question.answers.count >= 4
   end
 end

97f54df6385889241f83640b24a9d77bdd5f9247
ajluxey <v_koralina@list.ru>
2021-07-13 02:10:22 +0300

    Add shared helpers for current year and github url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  BASE_GITHUB_URL = URI('https://github.com')
+
+  def github_url(author, repo)
+    BASE_GITHUB_URL + (author + '/' + repo)
+  end
+
+  def year_now
+    Time.now.year
+  end
 end

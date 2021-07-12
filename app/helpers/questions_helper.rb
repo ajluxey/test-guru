adb2892584cd68075ce4ac9690e0fd234100a981
ajluxey <v_koralina@list.ru>
2021-07-13 02:18:11 +0300

    Delete return command

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,11 +2,11 @@ module QuestionsHelper
   def question_header(question, action)
     case action
     when :new
-      return "Create new question for #{question.test.title}"
+      "Create new question for #{question.test.title}"
     when :edit
-      return "Edit #{question.test.title} question"
+      "Edit #{question.test.title} question"
     else
-      return "Unknown action for #{question.test.title} question"
+      "Unknown action for #{question.test.title} question"
     end
   end
 end

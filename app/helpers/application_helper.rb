a95cae584cf2c110bf70bb1d542aed6be785bc4d
ajluxey <v_koralina@list.ru>
2021-07-14 17:31:51 +0300

    rework gihub url stringmaker

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,6 @@
 module ApplicationHelper
-  BASE_GITHUB_URL = URI('https://github.com')
-
   def github_url(author, repo)
-    link = BASE_GITHUB_URL + (author + '/' + repo)
+    link = "https://github.com/#{author}/#{repo}"
     link_to 'Github', link.to_s, target: '_blank', rel: 'nofollow'    
   end
 

e2e6b162e10a4af0e42640506a2fd5c5bc58af60
ajluxey <v_koralina@list.ru>
2021-07-14 14:58:36 +0300

    rework for correct working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,11 @@ module ApplicationHelper
   BASE_GITHUB_URL = URI('https://github.com')
 
   def github_url(author, repo)
-    BASE_GITHUB_URL + (author + '/' + repo)
+    link = BASE_GITHUB_URL + (author + '/' + repo)
+    link_to 'Github', link.to_s, target: '_blank', rel: 'nofollow'    
   end
 
   def year_now
-    Time.now.year
+    Time.current.year
   end
 end

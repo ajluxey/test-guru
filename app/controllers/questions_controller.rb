4ce36e7a0a9381921a69084cff7e2ed2bab1bb76
ajluxey <v_koralina@list.ru>
2021-07-14 14:31:24 +0300

    Correcting order of actions. Rework errors of exercise

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,13 @@
 class QuestionsController < ApplicationController
   
-  before_action :find_test, only: %i[index new create]
+  before_action :find_test, only: %i[new create]
   before_action :find_question, only: %i[show edit update destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def new
+    @question = @test.questions.new
+  end
 
   def create
     @question = @test.questions.new(question_params)
@@ -13,26 +17,18 @@ class QuestionsController < ApplicationController
       render :new
     end
   end
-
-  def destroy
-    @question.destroy
-    redirect_to test_questions_url(@question.test)
-  end
-
-  def index
-    @questions = @test.questions
-  end
-
-  def new
-    @question = @test.questions.new
-  end
-
+  
   def update
     if @question.update(question_params)
       redirect_to @question
     else
       render :edit
     end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to test_questions_url(@question.test)
   end
 
   private

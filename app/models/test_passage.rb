b3b345c57b38a79dab3f9335449b7ad54d158ea3
ajluxey <v_koralina@list.ru>
2021-07-17 02:37:26 +0300

    generate controller for test_passages and add methods for passing tests

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -2,4 +2,36 @@ class TestPassage < ApplicationRecord
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
+
+  before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_next_question, on: :update
+
+  def accept!(answer_ids)
+    if correct_answer?(answer_ids)
+      self.correct_questions += 1
+      save!
+    end
+  end
+
+  def complited?
+    current_question.nil?
+  end
+
+  private
+
+  def before_validation_set_first_question
+    self.current_question = test.questions.first if test.present?
+  end
+
+  def before_validation_set_next_question
+    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
+  end
+
+  def correct_answer?(answer_ids)
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+  end
+
+  def correct_answers
+    current_question.answers.correct
+  end
 end

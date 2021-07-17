37ddb051d8203c353be83fe44190563f4b025111
ajluxey <v_koralina@list.ru>
2021-07-17 12:24:31 +0300

    Move logic part to Model and use her methods

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,12 +1,10 @@
 module TestPassagesHelper
-  PASSAGE_TRESHOLD = 85
-
   def color_result(test_passage)
-    result = calculate_result(test_passage)
-    if result < PASSAGE_TRESHOLD
-      content_tag :span, "#{'%.2f' % result}%", style: 'color: red;'
-    else
+    result = test_passage.calculate_result
+    if test_passage.successfully_passed?
       content_tag :span, "#{'%.2f' % result}%", style: 'color: green;'
+    else
+      content_tag :span, "#{'%.2f' % result}%", style: 'color: red;'
     end
   end
 
@@ -15,20 +13,10 @@ module TestPassagesHelper
   end
 
   def passage_message(test_passage)
-    if calculate_result(test_passage) < PASSAGE_TRESHOLD
-      'Sorry, test failed. Learn more and try again!'
-    else
+    if test_passage.successfully_passed?
       'Congratulation! You passed this test!'
-    end
-  end
-
-  private
-
-  def calculate_result(test_passage)
-    if test_passage.correct_questions > 0
-      test_passage.correct_questions.to_f / test_passage.test.questions.count * 100
     else
-      0
+      'Sorry, test failed. Learn more and try again!'
     end
   end
 end

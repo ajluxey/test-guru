1533abb3c78d3e43e0a30ce52dda502fbf02e470
ajluxey <v_koralina@list.ru>
2021-07-17 12:25:45 +0300

    Add logic about test results

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -6,6 +6,8 @@ class TestPassage < ApplicationRecord
   before_validation :before_validation_set_first_question, on: :create
   before_validation :before_validation_set_next_question, on: :update
 
+  PASSAGE_TRESHOLD = 85
+
   def accept!(answer_ids)
     if correct_answer?(answer_ids)
       self.correct_questions += 1
@@ -15,6 +17,18 @@ class TestPassage < ApplicationRecord
 
   def complited?
     current_question.nil?
+  end
+
+  def successfully_passed?
+    calculate_result > PASSAGE_TRESHOLD
+  end
+
+  def calculate_result
+    if self.correct_questions > 0
+      self.correct_questions.to_f / self.test.questions.count * 100
+    else
+      0
+    end
   end
 
   private

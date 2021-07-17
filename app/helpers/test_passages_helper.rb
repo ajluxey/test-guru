916f49dd7b5e1e4ab02eac1026754060e44dbe86
ajluxey <v_koralina@list.ru>
2021-07-17 03:43:57 +0300

    Add helpers for colorize result and for result message

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,3 +1,30 @@
 module TestPassagesHelper
-  
+  PASSAGE_TRESHOLD = 85
+
+  def color_result(test_passage)
+    result = calculate_result(test_passage)
+    if result < PASSAGE_TRESHOLD
+      content_tag :span, "#{'%.2f' % result}%", style: 'color: red;'
+    else
+      content_tag :span, "#{'%.2f' % result}%", style: 'color: green;'
+    end
+  end
+
+  def passage_message(test_passage)
+    if calculate_result(test_passage) < PASSAGE_TRESHOLD
+      'Sorry, test failed. Learn more and try again!'
+    else
+      'Congratulation! You passed this test!'
+    end
+  end
+
+  private
+
+  def calculate_result(test_passage)
+    if test_passage.correct_questions > 0
+      test_passage.correct_questions.to_f / test_passage.test.questions.count * 100
+    else
+      0
+    end
+  end
 end

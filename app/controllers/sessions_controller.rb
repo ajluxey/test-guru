fbd01e529fac319f8f24ee637732afadcb12ee31
ajluxey <v_koralina@list.ru>
2021-07-19 01:58:29 +0300

    fixed bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
 
-  skip_before_action :authenticate_user!, only: %i[new create close]
+  skip_before_action :authenticate_user!, only: %i[new create destroy]
 
   def new
   end

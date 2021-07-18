b4a9c51eccd3bbf67c008c65c2461582ab06800f
ajluxey <v_koralina@list.ru>
2021-07-18 16:45:00 +0300

    Create helper logged_in? from controller method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base  
   
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
   before_action :authenticate_user!
 
   private
 
   def authenticate_user!
     unless current_user
-      redirect_to login_path
+      redirect_to login_path, alert: 'Are you a Guru? Verify your Email and Password please'
     end
   end
 

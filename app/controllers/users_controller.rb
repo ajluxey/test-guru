4fd8daf2f3e8dce307f9ac53124b9060f3aff276
ajluxey <v_koralina@list.ru>
2021-07-19 20:00:07 +0300

    Rework tests creation and adding user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
+      session[:user_id] = @user.id if @user.authenticate(user_params[:password])
       redirect_to tests_path
     else
       render :new

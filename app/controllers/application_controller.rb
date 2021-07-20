5da095719fecea420c038e288a58b64d2cdeaa5a
ajluxey <v_koralina@list.ru>
2021-07-20 06:44:54 +0300

    add root to admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(resource)
-    resource.is_a?(Admin) ? admin_tests_path : tests_path 
+    resource.is_a?(Admin) ? admin_tests_path : tests_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
+    root 'tests#index'
     resources :tests do
       resources :questions, except: :index, shallow: true do
         resources :answers, except: :index, shallow: true

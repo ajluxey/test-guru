8448a16e0e380751a6b76e3d350034601e26910e
ajluxey <v_koralina@list.ru>
2021-07-21 19:29:21 +0300

    Add link to flash message

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -4,7 +4,8 @@ class GistsController < ApplicationController
     gist_result = GistQuestionService.new(test_passage.current_question).call
 
     if gist_result.success?
-      flash_options = { notice: t('.success') + gist_result.url }
+      link = view_context.link_to('gists.github.com', gist_result.url, target:"_blank", rel: "nofollow")
+      flash_options = { notice: t('.success', link: link) }
       test_passage.user.gists.create!(question: test_passage.current_question,
                                       url: gist_result.url)
     else

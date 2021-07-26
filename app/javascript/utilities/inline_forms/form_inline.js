2a8b3c1099c1337443c4fc7cbcba80237b51a56b
ajluxey <v_koralina@list.ru>
2021-07-27 01:41:53 +0300

    Correct inline forms

--- app/javascript/utilities/inline_forms/form_inline.js
+++ app/javascript/utilities/inline_forms/form_inline.js
@@ -1,9 +1,9 @@
-export class FormInline {
+export default class FormInline {
   constructor (formInline) {
     this.formInline = formInline
     this.testId = formInline.dataset.testId
     this.testTitle = document.querySelector(`.test-title[data-test-id="${this.testId}"]`)
-    this.link = document.querySelector(`.form-inline-link[data-test-id="${testId}"]`)
+    this.link = document.querySelector(`.form-inline-link[data-test-id="${this.testId}"]`)
     
     this.withErrorsInDocument()
     this.link.addEventListener('click', (event) => {
@@ -25,9 +25,7 @@ export class FormInline {
   }
 
   withErrorsInDocument () {
-    const errors = document.querySelector('model-instance-errors')
-    if (errors) {
-      this.editHandler
-    }
+    const errors = document.querySelector('.model-instance-errors')
+    if (errors && errors.dataset.modelInstanceId == this.testId) this.editHandler()
   }
 }

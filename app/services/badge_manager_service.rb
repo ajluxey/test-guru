964c5aa15f049f1fa8a7ecad926eb18ea7e582b8
ajluxey <v_koralina@list.ru>
2021-07-29 15:02:46 +0300

    Correct Badge logic

--- app/services/badge_manager_service.rb
+++ app/services/badge_manager_service.rb
@@ -1,4 +1,4 @@
-class BadgeManager
+class BadgeManagerService
   def self.call(test_passage)
     self.new(test_passage)
   end
@@ -10,9 +10,9 @@ class BadgeManager
   private
 
   def check_and_award
-    Badge.all.each do { |badge|
+    Badge.all.each do |badge|
       rule_checker = Specifications::Badges::RULES[badge.rule].call(@test_passage, badge.rule, badge.rule_value)
       current_user.badges << badge if rule_checker.satisfied_with?(@test_passage) 
-    }
+    end
   end
 end
--- app/specifications/specifications.rb
+++ app/specifications/specifications.rb
@@ -1,10 +1,39 @@
-module Specification
+module Specifications
   module Badges
-    RULES = { first_try: FirstTryRule,
-              passed_all_tests_by_category: PassedAllTestsByCategoryRule,
-              passed_all_tests_by_level: PassedAllTestsByLevelRule }.freeze
-    
-    class FirstTryRule < AbstractBadgeRule
+    class AbstractRule
+      def initialize(rule, rule_value)
+        @rule = rule
+        @rule_value = rule_value
+      end
+
+      def satisfied_with?(test_passage)
+        test_passage.successfuly_passed?
+      end
+    end
+
+    class PassedAllTestsBySomeRule < AbstractRule
+      def satisfied_with?(test_passage, where_arguments)
+        return false unless super
+
+        event_start = Badges.find_by(rule: @rule, rule_value: @rule_value).updated_at
+        test_passages_after_badge_update = TestPassage.where(user: test_passage.current_user,
+                                                            current_question: nil,
+                                                            updated_at: event_start..Time.current)
+        # add scope to name
+        test_passages_by_some_rule = test_passages_after_badge_update.where(where_arguments)
+        if test_passages_by_some_rule.count > 0
+          successfully_passed_counts = Array.new(0, test_passages_by_some_rule.count)
+          test_passages_by_same_category.each_with_index do |tp, index|
+            successfully_passed_counts[index] += 1 if tp.successfully_passed?
+          end
+          successfully_passed_counts.all
+        else
+          false
+        end
+      end
+    end
+
+    class FirstTryRule < AbstractRule
       def satisfied_with?(test_passage)
         if super(test_passage)
           TestPassage.where(user: test_passage.user, test: test_passage.test).count == 1
@@ -34,39 +63,11 @@ module Specification
         end
       end
     end
-    
-    private
 
-    class AbstractBadgeRule
-      def initialize(rule, rule_value)
-        @rule = rule
-        @rule_value = rule_value
-      end
 
-      def satisfied_with?(test_passage)
-        test_passage.successfuly_passed?
-      end
-    end
+    RULES = { first_try: FirstTryRule,
+      passed_all_tests_by_category: PassedAllTestsByCategoryRule,
+      passed_all_tests_by_level: PassedAllTestsByLevelRule }.freeze
 
-    class PassedAllTestsBySomeRule < AbstractBadgeRule
-      def satisfied_with?(test_passage, where_arguments)
-        return false unless super
-
-        event_start = Badges.find_by(rule: @rule, rule_value: @rule_value).updated_at
-        test_passages_after_badge_update = TestPassage.where(user: test_passage.current_user,
-                                                            current_question: nil,
-                                                            updated_at: event_start..Time.current)
-        test_passages_by_some_rule = test_passages_after_badge_update.where(where_arguments)
-        if test_passages_by_some_rule.count > 0
-          successfully_passed_counts = Array.new(0, test_passages_by_some_rule.count)
-          test_passages_by_same_category.each_with_index do |tp, index|
-            successfully_passed_counts[index] += 1 if tp.successfully_passed?
-          end
-          successfully_passed_counts.all
-        else
-          false
-        end
-      end
-    end
   end
 end
